9f1a0459fe3cc1e93646bf20af4ea643d0cf13a1
DylanAbdou <dydy.licosa@gmail.com>
2018-11-20 11:41:25 +0100

    ex4 fix #3

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,51 @@
+package geco;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private LoginGenerator loginGenerator;
+    private LoginService loginService;
+    private String[] logins = {"JROL",
+            "BPER", "CGUR", "JDU", "JRAL", "JRAL1"};
+
+    @Before
+    public void setUp() throws Exception {
+        this.loginService = new LoginService(logins);
+        this.loginGenerator = new LoginGenerator(loginService);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.loginGenerator = null;
+        this.loginService = null;
+    }
+
+    @Test
+    public void testgenerateLoginForNomAndPrenomCT1() {
+        this.loginGenerator.generateLoginForNomAndPrenom("Durand","Paul");
+        assertTrue(this.loginService.loginExists("PDUR"));
+    }
+
+    @Test
+    public void testgenerateLoginForNomAndPrenomJohnRalling() {
+        this.loginGenerator.generateLoginForNomAndPrenom("Ralling","John");
+        assertTrue(this.loginService.loginExists("JRAL2"));
+    }
+
+    @Test
+    public void testgenerateLoginForNomAndPrenomCT2() {
+        this.loginGenerator.generateLoginForNomAndPrenom("Rolling","Jean");
+        assertTrue(this.loginService.loginExists("JROL1"));
+    }
+
+    @Test
+    public void testgenerateLoginForNomAndPrenomCT3() {
+        this.loginGenerator.generateLoginForNomAndPrenom("Dùrand","Paul");
+        assertTrue(this.loginService.loginExists("PDUR"));
+    }
+}
